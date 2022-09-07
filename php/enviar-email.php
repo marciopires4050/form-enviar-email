09e8222abe7a2093699692a574fc9d571bc2746f
Marcio Pires <pmbox80@gmail.com>
2022-09-07 11:44:24 -0300

    four commit

--- php/enviar-email.php
+++ php/enviar-email.php
@@ -25,12 +25,12 @@
   $assunto = "Contato pelo Site";
 
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
+  $headers = "MIME-Version: 1.0\n";
   $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+  $headers .= "From: $name <$email>";
 
   //Enviar
   mail($destino, $assunto, $arquivo, $headers);
   
-  echo "<meta http-equiv='refresh' content='10;URL=../marcioteste_form.html'>";
+  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
 ?>
